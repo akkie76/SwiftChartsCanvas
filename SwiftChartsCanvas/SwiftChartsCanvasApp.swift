2fa1d82a81c234d78b9c27adc1560973d39d4778
akihiko-sato <akihiko.sato@dena.com>
2025-04-06 22:09:15 +0900

    Initial Commit

---
+++ SwiftChartsCanvas/SwiftChartsCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsCanvasApp.swift
+//  SwiftChartsCanvas
+//
+//  Created by Akihiko Sato on 2025/04/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
